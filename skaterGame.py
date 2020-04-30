88e13438f8bab9f9b48383fd7babec13c1c70e08
renas <renasustek@gmail.com>
2020-04-30 20:50:32 +0100

    Add coin

--- skaterGame.py
+++ skaterGame.py
@@ -1,131 +1,172 @@
 import pygame
+import random
 
 pygame.init()
 BLACK = 0, 0, 0
-
+score = 0
 ####window####
-windowWidth = 704
-windowHeight = 416
-game_window = pygame.display.set_mode((windowWidth, windowHeight))
+window_width = 704
+window_height = 416
+game_window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("SKATER EXTREMMZ")
 
 clock = pygame.time.Clock()
 
 #### all the images ####
-walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
-             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
-             pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
-walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
-            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
-            pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+walk_right = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
+              pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+              pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walk_left = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
+             pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+             pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
 char = pygame.image.load('standing.png')
 bg = pygame.image.load("background.png")
 
-
 class Player:
-    def __init__(self, playerPosX, playerPosY, playerWidth, playerHeight):
-        self.playerPosX = playerPosX
-        self.playerPosY = playerPosY
-        self.playerWidth = playerWidth
-        self.playerHeight = playerHeight
+    def __init__(self, player_pos_x, player_pos_y, player_width, player_height):
+        self.player_pos_x = player_pos_x
+        self.player_pos_y = player_pos_y
+        self.player_width = player_width
+        self.player_height = player_height
         self.velocity = 5
         self.jump = False
-        self.jumpHeight = 10
-        self.maxJumpHeight = 10
+        self.jump_height = 10
+        self.max_jump_height = 10
         self.left = False
         self.right = False
-        self.walkCount = 0
+        self.walk_count = 0
         self.standing = True
-        self.hitbox = (self.playerPosX + 20, self.playerPosY, 20, 60)
+        self.hitbox = pygame.Rect(self.player_pos_x + 20, self.player_pos_y, 20, 60)
 
     def draw(self, window):
-        if self.walkCount + 1 >= 27:
-            self.walkCount = 0
+        if self.walk_count + 1 >= 27:
+            self.walk_count = 0
         if not self.standing:
             if self.left:
-                window.blit(walkLeft[self.walkCount // 3], (self.playerPosX, self.playerPosY))
-                self.walkCount += 1
+                window.blit(walk_left[self.walk_count // 3], (self.player_pos_x, self.player_pos_y))
+                self.walk_count += 1
             elif self.right:
-                window.blit(walkRight[self.walkCount // 3], (self.playerPosX, self.playerPosY))
-                self.walkCount += 1
+                window.blit(walk_right[self.walk_count // 3], (self.player_pos_x, self.player_pos_y))
+                self.walk_count += 1
         else:
             if self.right:
-                window.blit(walkRight[0], (self.playerPosX, self.playerPosY))
+                window.blit(walk_right[0], (self.player_pos_x, self.player_pos_y))
             else:
-                window.blit(walkLeft[0], (self.playerPosX, self.playerPosY))
-        self.hitbox = (self.playerPosX + 17, self.playerPosY + 2, 31, 57)  # NEW
+                window.blit(walk_left[0], (self.player_pos_x, self.player_pos_y))
+        self.hitbox = pygame.Rect(self.player_pos_x + 17, self.player_pos_y + 10, 31, 57)
         pygame.draw.rect(window, (255, 0, 0), self.hitbox, 1)
+
+    def movement(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT] and character.player_pos_x > character.velocity:
+            character.player_pos_x -= character.velocity
+            character.left = True
+            character.right = False
+            character.standing = False
+
+        elif keys[pygame.K_RIGHT] and character.player_pos_x < 852 - character.player_width - character.velocity:
+            character.player_pos_x += character.velocity
+            character.left = False
+            character.right = True
+            character.standing = False
+
+        else:
+            character.standing = True
+            character.walk_count = 0
+
+        if not character.jump:
+            if keys[pygame.K_UP]:
+                character.jump = True
+                character.left = False
+                character.right = False
+                character.walk_count = 0
+        else:
+            if character.jump_height >= -character.max_jump_height:
+                neg = 1
+                if character.jump_height < 0:
+                    neg = -1
+                character.player_pos_y -= (character.jump_height ** 2) * 0.5 * neg
+                character.jump_height -= 1
+            else:
+                character.jump = False
+                character.jump_height = character.max_jump_height
+
+    def collision(self, obs):
+        return self.hitbox.colliderect(obs)
+
+    def collision_occ(self):
+        self.player_pos_x = self.player_pos_x - 20
+        self.walk_count = 0
 
 
 class RectObstacle(pygame.Rect):
-    def __init__(self, obstacle_position_x, obstacle_position_y, obstacle_colour, obstacle_width, obstacle_height):
-        super().__init__(obstacle_position_x, obstacle_position_y, obstacle_width, obstacle_height)
-        # self.obstacle_position_x = obstacle_position_x
-        # self.obstacle_position_y = obstacle_position_y
-        self.obstacle_colour = obstacle_colour
-        # self.obstacle_height = obstacle_height
-        # self.obstacle_position_x = obstacle_position_x
-        # self.hitbox = (self.obstacle_position_x + 20, self.obstacle_position_y, 20, 60)
-        # self.image = pygame.Surface((obstacle_width, obstacle_height))
-        # self.image.fill(BLACK)
+    def __init__(self, x, y, colour, width, height):
+        super().__init__(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.colour = colour
 
     def draw(self, window):
-        pygame.draw.rect(window, self.obstacle_colour, self, 0)
+        pygame.draw.rect(window, self.colour, self, 0)
+
+
+class Coin(pygame.Rect):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.collided = False
+        self.coin_image = pygame.image.load('coin.png')
+        self.sound = pygame.mixer.Sound('coin.wav')
+
+    def collision(self, obs):
+        if self.collided:
+            return False
+        elif self.colliderect(obs):
+            self.sound.play()
+            self.collided = True
+            return True
+        else:
+            return False
+
+    def draw(self, window):
+        if not self.collided:
+            window.blit(self.coin_image, (self.x, self.y))
+
 
 character = Player(100, 265, 64, 64)
-wall = RectObstacle(200, 225, (0, 0, 0), 50, 50)
+wall = RectObstacle(200, 230, (13, 122, 35), 25, 105)
+coin = Coin(random.randint(0, window_width), 295, 16, 16)
 
 
-def redrawDisplay():
-    global walkCount
+def redraw_display():
     game_window.blit(bg, (0, 0))
     wall.draw(game_window)
+    coin.draw(game_window)
     character.draw(game_window)
     pygame.display.update()
 
 
 run = True
-
 while run:
     clock.tick(27)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    if character.collision(wall):
+        character.collision_occ()
+
+    coin.collision(character.hitbox)
+
     # controls
-    keys = pygame.key.get_pressed()
+    character.movement()
 
-    if keys[pygame.K_LEFT] and character.playerPosX > character.velocity:
-        character.playerPosX -= character.velocity
-        character.left = True
-        character.right = False
-        character.standing = False
-    elif keys[pygame.K_RIGHT] and character.playerPosX < 852 - character.playerWidth - character.velocity:
-        character.playerPosX += character.velocity
-        character.left = False
-        character.right = True
-        character.standing = False
-    else:
-        character.standing = True
-        character.walkCount = 0
-
-    if not character.jump:
-        if keys[pygame.K_UP]:
-            character.jump = True
-            character.left = False
-            character.right = False
-            character.walkCount = 0
-    else:
-        if character.jumpHeight >= -character.maxJumpHeight:
-            neg = 1
-            if character.jumpHeight < 0:
-                neg = -1
-            character.playerPosY -= (character.jumpHeight ** 2) * 0.5 * neg
-            character.jumpHeight -= 1
-        else:
-            character.jump = False
-            character.jumpHeight = character.maxJumpHeight
-
-    redrawDisplay()
+    redraw_display()
 
 pygame.quit()
